6bcae4bd277b24fb38072e49320b9ea4bf3ba8d3
Tonmecool <qpwoei.86@mail.ru>
2024-06-04 22:32:34 +0700

    add \n

--- handlers/converters/chats.py
+++ handlers/converters/chats.py
@@ -4,7 +4,7 @@ from dtos.messages import ChatListItemDTO
 def convert_chats_dtos_to_message(chats: list[ChatListItemDTO]) -> str:
     return '\n'.join(
         (
-            'Список всех доступных чатов:', '\n'.join(
+            'Список всех доступных чатов:\n', '\n'.join(
                 (
                     f'ChatOID: {chat.oid}. '
                     f'\nПроблема: {chat.title}\n'
